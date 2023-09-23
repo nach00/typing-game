69f68b6a51c93e4f0518ec330e3a636805fe29ee
nach00 <bignacho@gmail.com>
2023-09-23 14:39:23 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import tip from "./tip.wav";
 
 const data = {
   letters: "abcdefghijklmnopqrstuvwxyz".split(""),
-  numbers: "0123456789".split(""),
-  symbols: "<>;'\"[]{}+=()&%$#@!_-*:.,`?".split("")
+  // numbers: "0123456789".split(""),
+  // symbols: "<>;'\"[]{}+=()&%$#@!_-*:.,`?".split("")
 };
 
 const App = () => {
@@ -20,14 +20,8 @@ const App = () => {
 
       currentLetter = data.letters[Math.floor(Math.random() * data.letters.length)];
       
-      const playSound2 = () => {
-        const [play] = useSound(tip);
-        play();
-      };
-      
-      playSound2();
 
-      // console.log(currentLetter);
+      console.log(currentLetter);
 
       window.location.reload();
       
@@ -36,24 +30,6 @@ const App = () => {
     }
 
   };
-
-  // const handleLetterDisplay = () => {
-  //   return (
-  //     <div>
-  //       <h1>Press the letter: {currentLetter}</h1>
-  //     </div>
-  //   );
-  // };
-
-
-
-  // function renderDisplay() {
-  //   return (
-  //     <div>
-  //       <h1>Press the letter: {currentLetter}</h1>
-  //     </div>
-  //   );
-  // }
 
   const SetRandomBackgroundColor = () => {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
@@ -65,19 +41,9 @@ const App = () => {
     document.body.style.color = "#" + randomColor;
   };
 
-  const playSound = () => {
-    useSound(tip);
-  };
-
-  const playSound2 = () => {
-    const [play] = useSound(tip);
-    play();
-  };
-
 
 
   document.addEventListener("keydown", handleKeyDown);
-  // document.addEventListener("keydown", handleLetterDisplay);
 
   return (
     SetRandomTextColor(),
